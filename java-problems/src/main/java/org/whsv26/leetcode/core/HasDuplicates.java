f75f17a97ce637fb2944d8a5bc4db8cc0a7c4f58
whsv26 <whsv26@gmail.com>
2026-08-13 02:59:29 +0300

    Add has duplicates hash set training

--- java-problems/src/main/java/org/whsv26/leetcode/core/HasDuplicates.java
+++ java-problems/src/main/java/org/whsv26/leetcode/core/HasDuplicates.java
@@ -1,9 +1,22 @@
 void main() {
     assert !hasDuplicates(new int[]{1, 2, 3});
     assert hasDuplicates(new int[]{1, 2, 3, 2});
+
+    assert !hasDuplicates1(new int[]{1, 2, 3});
+    assert hasDuplicates1(new int[]{1, 2, 3, 2});
 }
 
 boolean hasDuplicates(int[] nums) {
+    var seen = new HashSet<Integer>();
+    for (var num : nums) {
+        if (!seen.add(num)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+boolean hasDuplicates1(int[] nums) {
     var seen = new HashSet<Integer>();
     for (var num : nums) {
         if (seen.contains(num)) {
