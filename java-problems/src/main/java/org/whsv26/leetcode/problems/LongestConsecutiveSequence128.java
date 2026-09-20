0af1c307b8f0f5b8d53ec4b1caf35332cc711fa2
whsv26 <whsv26@gmail.com>
2026-09-21 02:07:14 +0300

    128. LongestConsecutiveSequence

---
+++ java-problems/src/main/java/org/whsv26/leetcode/problems/LongestConsecutiveSequence128.java
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/longest-consecutive-sequence
+
+void main() {
+    assert 4 == new Solution().longestConsecutive(new int[]{100, 4, 200, 1, 3, 2});
+}
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        var numSet = new HashSet<Integer>();
+
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        int longest = 0;
+
+        for (int num : numSet) {
+            if (!numSet.contains(num - 1)) {
+                int length = 1;
+                while (numSet.contains(num + length)) {
+                    length++;
+                }
+                longest = Math.max(longest, length);
+            }
+        }
+
+        return longest;
+    }
+}
